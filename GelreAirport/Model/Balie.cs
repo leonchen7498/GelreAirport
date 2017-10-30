e1f1fed132e7caa60b7b51eac30c4c33a6e8141b
Leon Chen <leonchen@live.nl>
2017-10-30 14:13:50 +0100

    mooye models toegevoegd

---
+++ GelreAirport/Model/Balie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelreAirport.Model
+{
+    class Balie
+    {
+        private int balienummer;
+
+        public int Balienummer
+        {
+            get { return balienummer; }
+        }
+
+        public Balie(int balienummer)
+        {
+            this.balienummer = balienummer;
+        }
+    }
+}
---
+++ GelreAirport/Model/Object.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelreAirport.Model
+{
+    class Object
+    {
+        private int volgnummer;
+        private int passagiernummer;
+        private int vluchtnummer;
+        private int gewicht;
+
+        public int Volgnummer
+        {
+            get { return volgnummer; }
+        }
+
+        public int Passagiernummer
+        {
+            get { return passagiernummer; }
+        }
+
+        public int Vluchtnummer
+        {
+            get { return vluchtnummer; }
+        }
+
+        public int Gewicht
+        {
+            get { return gewicht; }
+        }
+
+        public Object(int volgnummer, int passagiernummer, int vluchtnummer, int gewicht)
+        {
+            this.volgnummer = volgnummer;
+            this.passagiernummer = passagiernummer;
+            this.vluchtnummer = vluchtnummer;
+            this.gewicht = gewicht;
+        }
+    }
+}
