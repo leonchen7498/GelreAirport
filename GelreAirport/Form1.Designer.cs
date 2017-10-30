ef27bbbf9b1a16c5f5eabea811db4833ef9a92fe
Leon Chen <leonchen@live.nl>
2017-10-30 13:47:26 +0100

    Wow geweldige commit waar je balies kan selecteren

--- GelreAirport/Form1.Designer.cs
+++ GelreAirport/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbBalies = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.SystemColors.Control;
+            this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.Location = new System.Drawing.Point(519, 384);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(151, 57);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "Ok";
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecteer uw balie.";
+            // 
+            // lbBalies
+            // 
+            this.lbBalies.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBalies.FormattingEnabled = true;
+            this.lbBalies.ItemHeight = 46;
+            this.lbBalies.Location = new System.Drawing.Point(12, 52);
+            this.lbBalies.Name = "lbBalies";
+            this.lbBalies.Size = new System.Drawing.Size(658, 326);
+            this.lbBalies.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.lbBalies);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConfirm);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbBalies;
     }
 }
 
